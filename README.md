834f4b3c6d3de035a2d9507555b0aa7e37176041
Stefan Malzner <stefan@adlk.io>
2017-11-17 21:13:51 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Slack for Franz
-This is the official Franz recipe for Slack
+# Zendesk for Franz
+This is the official Franz recipe for Zendesk
 
 ### How to create your own Franz recipes:
 * [Read the documentation](https://github.com/meetfranz/plugins)
--- webview.js
+++ webview.js
@@ -1,43 +1,13 @@
-import { ipcRenderer } from 'electron';
-import path from 'path';
-
-const getTeamIcon = function getTeamIcon(count = 0) {
-  console.log('getTeamIcon', count);
-  let countTeamIconCheck = count;
-  document.querySelector('#team_menu').click();
-  countTeamIconCheck += 1;
-  const icon = document.querySelector('.team_icon');
-  let bgUrl = window.getComputedStyle(icon, null).getPropertyValue('background-image');
-  bgUrl = /^url\((['"]?)(.*)\1\)$/.exec(bgUrl);
-  bgUrl = bgUrl ? bgUrl[2] : '';
-  if (bgUrl) {
-    ipcRenderer.sendToHost('avatar', bgUrl);
-  } else if (countTeamIconCheck <= 3) {
-    setTimeout(() => {
-      getTeamIcon(countTeamIconCheck + 1);
-    }, 1000);
-  }
-
-  setTimeout(() => {
-    document.querySelector('.team_menu').remove();
-    document.querySelector('#msg_input .ql-editor').focus();
-  }, 10);
-};
-
 module.exports = (Franz) => {
   const getMessages = () => {
-    const directMessages = document.querySelectorAll('.p-channel_sidebar__badge').length;
-    const allMessages = document.querySelectorAll('.p-channel_sidebar__channel--unread:not([class*="p-channel_sidebar__channel--muted"])').length - directMessages;
+    let count = 0;
+    const el = document.querySelector('.dashboard-top-panel .indicators .stats-group .cell-value');
+    if (el) {
+      count = parseInt(el.innerHTML, 10);
+    }
 
     // set Franz badge
-    Franz.setBadge(directMessages, allMessages);
+    Franz.setBadge(count);
   };
   Franz.loop(getMessages);
-
-  setTimeout(() => {
-    getTeamIcon();
-  }, 4000);
-
-  // inject franz.css stylesheet
-  Franz.injectCSS(path.join(__dirname, 'service.css'));
 };
